c731d04727161c5beea56387d70a280226272733
namnguyendsn <namnguyendsn@gmail.com>
2014-03-25 23:40:22 +0700

    remove

--- CPUSTM32_V1.1/jupiter firmware/Jupiter_config/jupiterHSICab.h
+++
@@ -1,7 +0,0 @@
-#include <stm32f10x.h>
-#include "common_config.h"
-#include "HSI_calibration.h"
-
-void jupiterHSICab_init(void);
-
-
--- CPUSTM32_V1.1/jupiter firmware/Jupiter_config/timer_delay.h
+++
@@ -1,3 +0,0 @@
-#include "common_config.h"
-
-void Delay_ms(uint32_t nTime);
